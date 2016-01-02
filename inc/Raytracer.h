c0a88529a775b39c47c4efe40561cc88d8477230
Luís Cláudio G. Rocha <luisclaudiogouveiarocha@gmail.com>
2016-01-02 19:30:09 -0300

    create rendering infrastructure

--- inc/Raytracer.h
+++ inc/Raytracer.h
@@ -7,8 +7,37 @@
 class Raytracer
 {
 private:
+
+	static Raytracer* raytracer_ptr;
+	SDL_Surface* surface;
+
+	void drawSurface(ImplicitSurface *s, 
+						vec_3d cam_pos, 
+						vec_3d cam_direction, 
+						vec_3d cam_up, 
+						double focal_d, 
+						int w, int h,
+						int nX, int nY);
+
+	//-------------------------
+	//--------- CTOR ----------
+	//-------------------------
+	Raytracer();
+
 public:
-	void drawSurface(ImplicitSurface* s);	
+	//-------------------------
+	//--------- DTOR ----------
+	//-------------------------
+	~Raytracer();
+
+	static Raytracer* instance() {
+		if(!Raytracer::raytracer_ptr)
+			raytracer_ptr = new Raytracer();
+		return Raytracer::raytracer_ptr;
+	}
+
+
+	void drawSurface(ImplicitSurface* s, int w, int h);	
 };
 
 #endif
--- src/Raytracer.cpp
+++ src/Raytracer.cpp
@@ -1,6 +1,51 @@
 #include "../inc/Raytracer.h"
 
-void Raytracer::drawSurface(ImplicitSurface* s)
+Raytracer* Raytracer::raytracer_ptr = 0;
+
+Raytracer::Raytracer()
 {
+	// initialize SDL 
+    SDL_Init(SDL_INIT_VIDEO); 
+
+    // set the title bar 
+    SDL_WM_SetCaption("SDL Test", "SDL Test"); 
+
+    // create window 
+    this->surface = SDL_SetVideoMode(600, 800, 32,
+                                       SDL_HWSURFACE|SDL_DOUBLEBUF);
+}
+
+Raytracer::~Raytracer()
+{
+	SDL_Quit();
+	delete Raytracer::raytracer_ptr;
+}
+
+void Raytracer::drawSurface(ImplicitSurface* s, int w, int h)
+{
+	return;
+}
+
+void Raytracer::drawSurface(ImplicitSurface *s, 
+	vec_3d cam_pos, 
+	vec_3d cam_direction, 
+	vec_3d cam_up, 
+	double focal_d, 
+	int w, int h,
+	int nX, int nY)
+{
+
+	//Loop through pixels
+	for(int i = 0; i < nX; i++)
+		for(int j = 0; j < nY; j++)
+		{
+			double u, v;
+			//pixel_position_cam_frame( );
+
+			//Uint32 p = throw_ray();
+
+
+		}
+
 	return;
 }
