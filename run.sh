1bd154f430f55f4839d50380d54445a470862aea
Luís Cláudio G. Rocha <luisclaudiogouveiarocha@gmail.com>
2016-01-01 21:15:26 -0300

    + link SDL library while compiling

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-g++ -O0 -g -std=c++11 -o ./tests/bin/test_vdwSurface ./src/*.cpp ./tests/test_vdwSurface.cpp
+g++ -O0 -g -std=c++11 -lm -lSDL -o ./tests/bin/test_vdwSurface ./src/*.cpp ./tests/test_vdwSurface.cpp
 
 
-g++ -O0 -g -std=c++11 -o dock ./src/*.cpp main.cpp
+g++ -O0 -g -std=c++11 -lm -lSDL -o dock ./src/*.cpp main.cpp
