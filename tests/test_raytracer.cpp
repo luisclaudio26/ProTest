6cebf127494c5091efacab85476ac1dcd2a8e7b8
Luís Cláudio G. Rocha <luisclaudiogouveiarocha@gmail.com>
2016-01-03 15:28:50 -0300

    adjust parameters so to avoid distortions on image

--- tests/test_raytracer.cpp
+++ tests/test_raytracer.cpp
@@ -5,10 +5,13 @@ int main()
 {
 	Atom a; a.setElement( C ); 
 			a.setPosition( (vec_3d){0.0, 0.0, 0.0} );
-	
+	Atom b; b.setElement( O );
+			b.setPosition( (vec_3d){0.9, 0.0, 0.0} );
+
 	VDWSurface s; 
 	s.push_atom(a);
+	s.push_atom(b);
 
-	Raytracer::instance()->drawSurface(&s, 800, 600);
+	Raytracer::instance()->drawSurface(&s, 600, 600);
 	return 0;
 }
